e6c65d0850c1f9304302efb8a5fe18a2c23e0cef
sujit <sujitramdhakal59@gmail.com>
2023-11-02 21:33:06 +0545

    form and table completed

--- students/views.py
+++ students/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .models import Student
 
 def index(request):
-    return render(request,'index.html')
+
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        age = request.POST.get("age")
+        address = request.POST.get("address")
+
+        student = Student.objects.create(name=name,age=age,address=address)
+        student.save()
+        return redirect('/index/')
+
+    students = Student.objects.all()
+    context = {
+        'students':students,
+    }
+
+    return render(request,'index.html',context)
